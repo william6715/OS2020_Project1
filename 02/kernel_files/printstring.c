3e14be2baf80284b3e18f4b9cd643cd634f8d86c
unknown <b05202061@ntu.edu.tw>
2020-04-27 21:51:08 +0800

    change kernel mode

--- 02/kernel_files/printstring.c
+++ 02/kernel_files/printstring.c
@@ -2,22 +2,15 @@
 #include <linux/linkage.h>
 #include <linux/syscalls.h>
 #include <linux/uaccess.h>
+#include <linux/ktime.h>
+#include <linux/timekeeping.h>
 
-
-asmlinkage long sys_printstring(int len, char __user * src)
+asmlinkage long sys_printdmesg(int len, char __user * src)
 {
 	char buf[256];
 	unsigned long lenleft = len;
-	unsigned long chunklen = sizeof(buf);//256
-
-	while( lenleft > 0 ) {
-		if ( lenleft < chunklen )
-			chunklen = lenleft;
-		if ( copy_from_user(buf, src, chunklen) )//copy form user to kernel, return 0 mean success
-			return -EFAULT;
-
-		lenleft -= chunklen;
-		printk("%s", buf);
-	}
+	//copy form user to kernel, return 0 mean success
+	if ( copy_from_user(buf, src, lenleft) )return -EFAULT;
+	printk("%s", buf);
 	return 0;
 }
--- 02/kernel_files/syscalls.h
+++ 02/kernel_files/syscalls.h
@@ -940,5 +940,5 @@ asmlinkage long sys_pkey_alloc(unsigned long flags, unsigned long init_val);
 asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
-asmlinkage long sys_printstring(int len, char __user * src);
+asmlinkage long sys_printdmesg(int len, char __user * src);
 #endif
