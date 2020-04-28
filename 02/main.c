bc433e23ef99438faeb14c38cdf4387b28e60ccf
unknown <b05202061@ntu.edu.tw>
2020-04-28 21:15:17 +0800

    change main

--- 02/main.c
+++ 02/main.c
@@ -8,18 +8,17 @@
 
 
 int main(){
-	char SP[8];
-	int n;
-	scanf("%s", SP);
-	scanf("%d", &n);
-	Process* proc = malloc(n * sizeof(Process));
+	char sched[8];
+	int N;
+	scanf("%s", sched);
+	scanf("%d", &N);
+	Process* process = malloc(N * sizeof(Process));
 	//it will be sort by ready time already
-	for(int i = 0 ; i < n ; i++){
-        // assign tmp to each 'Process' 
-		Process *tmp = (proc + i);
-		scanf("%s %d %d", tmp -> name, &(tmp -> ready_time), &(tmp -> exec_time));
-		tmp -> pid = -1; 
-		if(pipe((tmp -> pipe_fd)) < 0){
+	for(int i = 0 ; i < N ; i++){
+		//initial 
+		scanf("%s %d %d", process[i].name, &(process[i].ready_time), &(process[i].exec_time));
+		process[i].pid = -1; 
+		if(pipe((process[i].pipe_fd)) < 0){
 			perror("error: pipe");
 			exit(1);
 		}
@@ -29,10 +28,10 @@ int main(){
 	// my virtual box have 4 cpu
 	assign_core(getpid(), SCHED_CORE);
 	
-	if(strcmp(SP, "FIFO") == 0) scheduler_FIFO(proc, n);
-	else if(strcmp(SP, "RR") == 0) scheduler_RR(proc, n);
-	else if(strcmp(SP, "SJF") == 0) scheduler_SJF(proc, n);
-	else if(strcmp(SP, "PSJF") == 0) scheduler_PSJF(proc, n);
-	else printf("No Such scheduler policy.\n");
+	if(strcmp(sched, "FIFO") == 0) scheduler_FIFO(process, N);
+	else if(strcmp(sched, "RR") == 0) scheduler_RR(process, N);
+	else if(strcmp(sched, "SJF") == 0) scheduler_SJF(process, N);
+	else if(strcmp(sched, "PSJF") == 0) scheduler_PSJF(process, N);
+	else printf("No Such scheduler method.\n");
 	return 0;
 }
